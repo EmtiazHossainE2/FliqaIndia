94aebbfab9f591d194554702aa447f3d58bca351
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-06-15 16:10:05 +0600

    brand section

--- src/components/Homepage/Brands.jsx
+++ src/components/Homepage/Brands.jsx
@@ -1,8 +1,18 @@
 
 const Brands = () => {
   return (
-    <div>Brands</div>
-  )
+    <div className="customContainer py-8">
+      <div className="lg:flex lg:flex-row justify-center gap-16 font-[Homenaje] text-2xl ">
+        <h2 className="pb-5 lg:pb-0 font-bold lg:font-normal">Popular: </h2>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-5">
+          <h2 className="text-[#4C696D]">Website Design</h2>
+          <h2 className="text-[#4C696D]">Logo Design</h2>
+          <h2 className="text-[#4C696D]">Brochure</h2>
+          <h2 className="text-[#4C696D]">Label Design</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Brands
